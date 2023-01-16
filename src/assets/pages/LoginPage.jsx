82500c4a6a806529a304afff177630f50804b2c9
Wahed Ali <wahed.khan@rediffmail.com>
2023-01-16 12:46:53 +0530

    admin remark bug fix

--- src/assets/pages/LoginPage.jsx
+++ src/assets/pages/LoginPage.jsx
@@ -65,7 +65,7 @@ const LoginPage = () => {
       password,
       role: 'user',
       filepath: 'somefile.txt',
-      remark: 'Approval Pending',
+      remark: 'Approval pending',
       isApproved: 'Pending',
     });
     localStorage.setItem('users', JSON.stringify(users));
